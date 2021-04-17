d7c56ebcd1e128d4775c7ed3d5659078d96b1fde
Gavrilo Jovanovic <Jovanovic.Nemanjic@gmail.com>
2021-04-17 21:16:11 +0200

    Povezan klijent i server za log in i registraciju

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,9 +5,9 @@ const controllerUser = require('../controllers/users');
 const controllerRegions = require('../controllers/regions');
 const upload = require('../upload/uploadImage')
 
-router.post('/newUser', upload.single('image'), controllerUser.addNewUser);
-router.post('/signIn', controllerUser.findUser);
-router.post('/addRegionForUser', controllerUser.addRegionForUser);
+router.post('/:newUser', upload.single('image'), controllerUser.addNewUser);
+router.post('/:signIn', controllerUser.findUser); 
+router.post('/:addRegionForUser', controllerUser.addRegionForUser);
 
 router.get('/:region_name', controllerRegions.getRegionFacts);
 
