c846e61c911ebad700e74c19cc466ab7a3f0e40d
Gavrilo Jovanovic <Jovanovic.Nemanjic@gmail.com>
2021-04-23 12:10:44 +0200

    Ispravljanje linija koda u comment.js shemi

--- server/models/comments.js
+++ server/models/comments.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const commentsSchema = mongoose.Schema({
+const commentSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     region: {
         type: mongoose.Schema.Types.ObjectId,
