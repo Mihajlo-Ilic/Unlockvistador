2b73bb8b10eb9a8d6862ddec3e1a6b02e808bcfb
Darinka Nesovic <darinka.nesovic@rt-rk.com>
2021-05-14 11:52:04 +0200

    Changed look of main page

--- client/src/app/main-page/main-page.component.ts
+++ client/src/app/main-page/main-page.component.ts
@@ -7,9 +7,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  	constructor() { }
 
-  ngOnInit(): void {
-  }
+  	ngOnInit(): void {
+  	}
+
+  	public sendGetReq(ime_oblasti) {
+        
+    }
+
+    public clickHandlerBanat() {
+    }
+    public mouseOverHandlerBanat() {
+        
+    }
+    public mouseOutHandlerBanat() {
+        
+    }
+
+    public clickHandlerBacka() {
+        
+    }
+    public mouseOverHandlerBacka() {
+        
+    }
+    public mouseOutHandlerBacka() {
+        
+    }
+
+    public clickHandlerSrem() {
+      
+    }
+    public mouseOverHandlerSrem() {
+       
+    }
+    public mouseOutHandlerSrem() {
+       
+    }
+
+    public clickHandlerBeograd() {
+      
+    }
+    public mouseOverHandlerBeograd() {
+       
+    }
+    public mouseOutHandlerBeograd() {
+       
+    }
+    
+    public clickHandlerIstok() {
+      }
+    public mouseOverHandlerIstok() {
+      
+    }
+    public mouseOutHandlerIstok() {
+        
+    }
+
+    public clickHandlerSumadija() {
+        
+    }
+    public mouseOverHandlerSumadija() {
+       
+    }
+    public mouseOutHandlerSumadija() {
+        
+    }
+
+    public clickHandlerNisava() {
+        
+    }
+    public mouseOverHandlerNisava() {
+       
+    }
+    public mouseOutHandlerNisava() {
+       
+    }
+
+    public clickHandlerRaska() {
+       
+    }
+    public mouseOverHandlerRaska() {
+       
+    }
+    public mouseOutHandlerRaska() {
+       
+    }
+
+    public clickHandlerKosovo() {
+       
+    }
+    public mouseOverHandlerKosovo() {
+      
+    }
+    public mouseOutHandlerKosovo() {
+    }
 
 }
